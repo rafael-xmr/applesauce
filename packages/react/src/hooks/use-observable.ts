a6904afecd8884d486a229ce087e9ca40bfc4e3f
hzrd149 <github@hzrd149.com>
2024-11-26 13:52:09 -0600

    fix useObservable not updating on change

--- packages/react/src/hooks/use-observable.ts
+++ packages/react/src/hooks/use-observable.ts
@@ -13,15 +13,19 @@ export function useObservable<T extends unknown>(observable?: BehaviorSubject<T>
 export function useObservable<T extends unknown>(observable?: Observable<T>): T | undefined;
 export function useObservable<T extends unknown>(observable?: Observable<T>): T | undefined {
   const current = observable && getCurrentValue(observable);
+  const [_count, setCount] = useState(0);
   const [value, setValue] = useState(current);
 
   useEffect(() => {
     // Reset the state, the method passed to subscribe will NOT always be called
     setValue(observable && getCurrentValue(observable));
 
-    const sub = observable?.subscribe(setValue);
+    const sub = observable?.subscribe((v) => {
+      setValue(v);
+      setCount((c) => c + 1);
+    });
     return () => sub?.unsubscribe();
-  }, [observable, setValue]);
+  }, [observable, setValue, setCount]);
 
   return current || value;
 }
