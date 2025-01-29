7bdcd21086833a7d1011c0430f8e8e3355418934
hzrd149 <github@hzrd149.com>
2025-01-29 12:30:51 -0600

    fix build

--- packages/react/src/hooks/use-active-account.ts
+++ packages/react/src/hooks/use-active-account.ts
@@ -2,7 +2,7 @@ import { useObservable } from "./use-observable.js";
 import { IAccount } from "applesauce-accounts";
 import { useAccountManager } from "./use-account-manager.js";
 
-export function useActiveAccount(): IAccount | null {
+export function useActiveAccount(): IAccount | undefined {
   const manager = useAccountManager();
   return useObservable(manager.active$);
 }
