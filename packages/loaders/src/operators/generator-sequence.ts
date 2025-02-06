8d0ecd736dafe362aaaa0d2d2ccf3719478ac4d4
hzrd149 <github@hzrd149.com>
2025-02-06 08:54:35 -0600

    fix tests

--- packages/loaders/src/operators/generator-sequence.ts
+++ packages/loaders/src/operators/generator-sequence.ts
@@ -10,51 +10,43 @@ export function generatorSequence<Input, Result>(
 ): OperatorFunction<Input, Result> {
   return (source) => {
     return new Observable<Result>((observer) => {
-      let complete = false;
+      const sub = source.subscribe((value) => {
+        const generator = createGenerator(value);
 
-      const sub = source.subscribe({
-        next: (value) => {
-          const generator = createGenerator(value);
+        const nextSequence = (prevResults?: Result[]) => {
+          const p = generator.next(prevResults);
 
-          const nextSequence = (prevResults?: Result[]) => {
-            const p = generator.next(prevResults);
+          const handleResult = (result: IteratorResult<Observable<Result>>) => {
+            // generator complete, exit
+            if (result.done) return observer.complete();
 
-            const handleResult = (result: IteratorResult<Observable<Result>>) => {
-              // generator complete, exit
-              if (result.done) return observer.complete();
-
-              const results: Result[] = [];
-              result.value.subscribe({
-                next: (v) => {
-                  // track results and pass along values
-                  results.push(v);
-                  observer.next(v);
-                },
-                error: (err) => {
-                  observer.error(err);
-                },
-                complete: () => {
-                  // run next step
-                  nextSequence(results);
-                },
-              });
-            };
-
-            // if its an async generator, wait for the promise
-            if (p instanceof Promise)
-              p.then(handleResult, (err) => {
+            const results: Result[] = [];
+            result.value.subscribe({
+              next: (v) => {
+                // track results and pass along values
+                results.push(v);
+                observer.next(v);
+              },
+              error: (err) => {
                 observer.error(err);
-              });
-            else handleResult(p);
+              },
+              complete: () => {
+                // run next step
+                nextSequence(results);
+              },
+            });
           };
 
-          // start running steps
-          nextSequence();
-        },
-        complete: () => {
-          // source is complete
-          complete = true;
-        },
+          // if its an async generator, wait for the promise
+          if (p instanceof Promise)
+            p.then(handleResult, (err) => {
+              observer.error(err);
+            });
+          else handleResult(p);
+        };
+
+        // start running steps
+        nextSequence();
       });
 
       return () => sub.unsubscribe();
