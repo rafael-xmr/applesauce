5e04ac0bc6b1ef3feaa648dbc59544894508059a
hzrd149 <github@hzrd149.com>
2025-03-15 14:12:44 +0000

    support passing events to follow set actions

--- packages/actions/src/actions/follow-sets.ts
+++ packages/actions/src/actions/follow-sets.ts
@@ -1,11 +1,14 @@
-import { kinds } from "nostr-tools";
+import { kinds, NostrEvent } from "nostr-tools";
 import { addPubkeyTag, removePubkeyTag } from "applesauce-factory/operations/tag";
 import { IEventStore } from "applesauce-core";
 
 import { Action } from "../action-hub.js";
 
-function getFollowSetEvent(events: IEventStore, self: string, identifier: string) {
-  return events.getReplaceable(kinds.Followsets, self, identifier);
+function getFollowSetEvent(events: IEventStore, self: string, identifier: NostrEvent | string) {
+  const set = typeof identifier === "string" ? events.getReplaceable(kinds.Followsets, self, identifier) : identifier;
+  if (!set) throw new Error("Can't find follow set");
+  if (set.kind !== kinds.Followsets) throw new Error("Event is not a follow set");
+  return set;
 }
 
 /**
@@ -14,10 +17,9 @@ function getFollowSetEvent(events: IEventStore, self: string, identifier: string
  * @param identifier the "d" tag of the follow set
  * @param hidden set to true to add to hidden follows
  */
-export function AddUserToFollowSet(pubkey: string, identifier: string, hidden = false): Action {
+export function AddUserToFollowSet(pubkey: string, identifier: NostrEvent | string, hidden = false): Action {
   return async function* ({ events, factory, self }) {
     const follows = getFollowSetEvent(events, self, identifier);
-    if (!follows) throw new Error("Can't find follow set");
 
     const operation = addPubkeyTag({ pubkey });
 
@@ -32,10 +34,9 @@ export function AddUserToFollowSet(pubkey: string, identifier: string, hidden = 
  * @param identifier the "d" tag of the follow set
  * @param hidden set to true to remove from hidden follows
  */
-export function RemoveUserFromFollowSet(pubkey: string, identifier: string, hidden = false): Action {
+export function RemoveUserFromFollowSet(pubkey: string, identifier: NostrEvent | string, hidden = false): Action {
   return async function* ({ events, factory, self }) {
     const follows = getFollowSetEvent(events, self, identifier);
-    if (!follows) throw new Error("Can't find follow set");
 
     const operation = removePubkeyTag(pubkey);
 
